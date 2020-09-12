172253eff7961684a15674fd84a3b11c19ca67fc
Bruno Cesar Dias <bcdbruno@gmail.com>
2020-09-12 20:20:05 -0300

    Build an API for Movies and implement delete customer

--- Vidly/Dtos/MovieDto.cs
+++ Vidly/Dtos/MovieDto.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Vidly.Models;
 
-namespace Vidly.Models
+namespace Vidly.Dtos
 {
-    public class Movie
+    public class MovieDto
     {
         public int Id { get; set; }
 
@@ -14,19 +15,13 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
-
         [Required]
-        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
-        [Required]
-        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
                 
-        [Display(Name = "Number in Stock")]
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
